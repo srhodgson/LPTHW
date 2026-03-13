80a9c33b4928c9c6552865c33dcbfdd2125035c4
Stephen Hodgson <stephen@srhodgson.co.uk>
2026-03-13 22:42:49 +0000

    flying through the basics so far
    Signed-off-by: Stephen Hodgson <stephen@srhodgson.co.uk>

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Stephen Hodgson'
+my_age = 41
+my_height = 178 # centimeters 
+my_weight = 118 # Kg
+my_eyes = 'Hazel'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} centimeters tall.")
+print(f"He's {my_weight} kilograms heavy")
+print("Actually that's not too heavy (it is).")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee.")
+
+# this line is tricky, try to get it right 
+total = my_age + my_height + my_weight 
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}")
